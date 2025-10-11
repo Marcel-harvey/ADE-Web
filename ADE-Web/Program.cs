bb39989f34ab8908687429a5f1f8ab71869687e7
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-11 17:35:04 +0200

    Added Identity framework with user login and register only

--- ADE-Web/Program.cs
+++ ADE-Web/Program.cs
@@ -9,65 +9,81 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 2. Add Identity
+// 2. Add Identity with roles
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-    options.SignIn.RequireConfirmedAccount = false) // set to true if you want email confirmation
+    options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-// Add services to the container.
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+
+// Add services to the container
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Admin User
+// Create Admin User
 async Task CreateAdminUser(IApplicationBuilder app)
 {
     using var scope = app.ApplicationServices.CreateScope();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-    // Create a default Admin user
     var adminEmail = "marcel@ade.com";
     var adminPassword = "Admin123!";
+    var adminRole = "Admin";
 
-    // Check if admin user has been created
-    if (await userManager.FindByEmailAsync(adminEmail) == null)
+    // Create role if not exists
+    if (!await roleManager.RoleExistsAsync(adminRole))
+        await roleManager.CreateAsync(new IdentityRole(adminRole));
+
+    // Create admin user if not exists
+    var user = await userManager.FindByEmailAsync(adminEmail);
+    if (user == null)
     {
-        var adminUser = new IdentityUser
+        user = new IdentityUser
         {
             UserName = adminEmail,
             Email = adminEmail,
             EmailConfirmed = true
         };
 
-        var result = await userManager.CreateAsync(adminUser, adminPassword);
+        await userManager.CreateAsync(user, adminPassword);
+        await userManager.AddToRoleAsync(user, adminRole);
     }
 }
 
 await CreateAdminUser(app);
 
-
-// Configure the HTTP request pipeline.
+// Middleware setup
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-// Setup dot allowing comma and dot notation for decimals, will save as dot (.) in models
+// Setup culture
 var defaultCulture = new CultureInfo("en-ZA");
 defaultCulture.NumberFormat.NumberDecimalSeparator = ".";
 defaultCulture.NumberFormat.CurrencyDecimalSeparator = ".";
 defaultCulture.NumberFormat.NumberGroupSeparator = ",";
+CultureInfo.DefaultThreadCurrentCulture = defaultCulture;
+CultureInfo.DefaultThreadCurrentUICulture = defaultCulture;
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
