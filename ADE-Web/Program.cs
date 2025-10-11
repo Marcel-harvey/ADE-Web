e503d43f4a08108032d8fef06498a79786d0bbac
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-11 16:11:00 +0200

    Added SQL Server Update

--- ADE-Web/Program.cs
+++ ADE-Web/Program.cs
@@ -1,9 +1,51 @@
+using ADE_Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Add SQL Server connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. Add Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    options.SignIn.RequireConfirmedAccount = false) // set to true if you want email confirmation
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Admin User
+async Task CreateAdminUser(IApplicationBuilder app)
+{
+    using var scope = app.ApplicationServices.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    // Create a default Admin user
+    var adminEmail = "marcel@ade.com";
+    var adminPassword = "Admin123!";
+
+    // Check if admin user has been created
+    if (await userManager.FindByEmailAsync(adminEmail) == null)
+    {
+        var adminUser = new IdentityUser
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(adminUser, adminPassword);
+    }
+}
+
+await CreateAdminUser(app);
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +54,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Setup dot allowing comma and dot notation for decimals, will save as dot (.) in models
+var defaultCulture = new CultureInfo("en-ZA");
+defaultCulture.NumberFormat.NumberDecimalSeparator = ".";
+defaultCulture.NumberFormat.CurrencyDecimalSeparator = ".";
+defaultCulture.NumberFormat.NumberGroupSeparator = ",";
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
