9b714df3529b69930d7ea3a19eef9154dc90bb8c
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-11 22:38:37 +0200

    Initial commit

---
+++ ADE-Web/Controllers/DashboardController.cs
@@ -0,0 +1,52 @@
+﻿using ADE_Web.Data;
+using ADE_Web.Models;
+using ADE_Web.Models.ViewModels;
+using ADE_Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ADE_Web.Controllers
+{
+    public class DashboardController :Controller
+    {
+        private readonly IAppsService _appsService;
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(IAppsService appsService, ApplicationDbContext context)
+        {
+            _appsService = appsService;
+            _context = context;
+        }
+
+        // View all current apps
+        public IActionResult Index()
+        {
+            var apps = _appsService.GetAllApps();
+
+            return View(apps);
+        }
+
+        // GET: Create a new app
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Create a new app
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateAppsBuildViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _appsService.AddApp(model);
+
+            TempData["Success"] = "App created successfully!";
+            return RedirectToAction(nameof(Index));            
+        }
+
+    }
+}
