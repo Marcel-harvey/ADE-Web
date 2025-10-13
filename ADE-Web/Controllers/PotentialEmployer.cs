2f30eb066ac08f1916f86524b1c944a7214bcbc8
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-13 11:45:30 +0200

    Added get tech from model

--- ADE-Web/Controllers/PotentialEmployer.cs
+++ ADE-Web/Controllers/PotentialEmployer.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ADE_Web.Models;
+using ADE_Web.Models.ViewModels;
+using ADE_Web.Services.TechStackService;
+using ADE_Web.Data;
 
 namespace ADE_Web.Controllers
 {
     public class PotentialEmployer : Controller
     {
-        public PotentialEmployer() { }
+        private readonly ITechService _techService;
+        private readonly ApplicationDbContext _context;
+
+        public PotentialEmployer(ITechService techService, ApplicationDbContext context) 
+        {
+            _techService = techService;
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var techStack = _techService.GetAllTech();
+
+            return View(techStack);
         }
     }
 }
