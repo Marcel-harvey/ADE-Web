62ded49f2912119b15662f644bc1b9429ae254b7
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-13 07:59:01 +0200

    Mover to own controller for future scaling

---
+++ ADE-Web/Controllers/FreelancingController.cs
@@ -0,0 +1,17 @@
+﻿using ADE_Web.Data;
+using ADE_Web.Models;
+using ADE_Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADE_Web.Controllers
+{
+    public class FreelancingController : Controller
+    {
+        public FreelancingController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ADE-Web/Controllers/PotentialEmployer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADE_Web.Controllers
+{
+    public class PotentialEmployer : Controller
+    {
+        public PotentialEmployer() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
