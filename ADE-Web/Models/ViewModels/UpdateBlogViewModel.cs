0654a89319aedd1120043ff8c619e5a41250c4cb
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-15 12:53:30 +0200

    Added ViewModal for update - id filed!

---
+++ ADE-Web/Models/ViewModels/UpdateBlogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ADE_Web.Models.ViewModels
+{
+    public class UpdateBlogViewModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; } = string.Empty;
+        public string BlogContent { get; set; } = string.Empty;
+    }
+}
