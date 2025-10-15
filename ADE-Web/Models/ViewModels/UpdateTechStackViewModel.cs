b66e2772f82b459ad12b5b5e6b37369ea8376ab7
Marcel Harvey <marcelharvey1@gmail.com>
2025-10-15 15:18:58 +0200

    Created ViewModel for tech stack

---
+++ ADE-Web/Models/ViewModels/UpdateTechStackViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADE_Web.Models.ViewModels
+{
+    public class UpdateTechStackViewModel
+    {
+        public int TechStackId { get; set; }
+        public string Language { get; set; } = string.Empty;
+        [Range(0, 5, ErrorMessage = "Value must be between 0 and 5!")]
+        public int Proficiency { get; set; }
+    }
+}
